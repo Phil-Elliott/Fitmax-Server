88cf78096c2777341ea5537ee59c8fd4fb0f63a6
Phil <phillippelliott@gmail.com>
2021-12-11 06:53:48 -0500

    created a get and a post req for the goals table

--- server.js
+++ server.js
@@ -108,28 +108,63 @@ app.put("/run", (req, res) => {
     .catch((err) => res.status(400).json("unable to add data"))
 })
 
+app.post("/goals", (req, res) => {
+  const { email, distance, calories, time } = req.body
+  db.select("*")
+    .from("goals")
+    .where({ email })
+    .then((data) => {
+      if (data.length) {
+        db("goals")
+          .where({ email })
+          .update({
+            distance: distance,
+            calories: calories,
+            speed: time,
+          })
+          .then(res.json("updated"))
+          .catch((err) => res.status(400).json("unable to update data"))
+      } else {
+        db("goals")
+          .insert({
+            email: email,
+            distance: distance,
+            calories: calories,
+            speed: time,
+          })
+          .then(res.json("added"))
+          .catch((err) => res.status(400).json("unable to add data"))
+      }
+    })
+})
+
+app.get("/goals/", (req, res) => {
+  const { email } = req.query
+  db.select("*")
+    .from("goals")
+    .where({ email })
+    .then((data) => {
+      if (data.length) {
+        res.json(data)
+      } else {
+        res.status(400).json("goals not found")
+      }
+    })
+    .catch((err) => res.status(400).json("error getting goals"))
+})
+
 app.listen(3001, () => {
   console.log("its running on port 3001")
 })
 
 /*
-  1) First entry ever doesnt show up 
-    - Seems like refresh isnt running for that 1st entry 
-    - maybe try learning more about promises 
-  2) Fix deletions 
-    - Only works for first 2 or 3 
-    - Have to double click sometimes
-    - try catching the error
-
-    - I think it is working but it stops updating 
   3) Add goals 
     - Make a goals table 
       - email, distance, speed, calories 
-      - grab at the beginning 
-        - look into doing multiple fetches at once 
+      - same as adding a run but you are going to alter the table 
+        - look into doing multiple fetches at once (promise all)
+
+        can use an if statement in case they have one 
+        if they have an entry then alter, if not then create new
   4) Clean up the code and put in some notes 
-
-
-
-  change onclick for daily to go back to main page and refresh runs which will cause the useEffect daily to get triggered
 */
