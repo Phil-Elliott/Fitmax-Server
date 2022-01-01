7b0b1312b329cb55e5a6a348d50d13d19d66be08
Phil <phillippelliott@gmail.com>
2022-01-01 04:15:10 -0500

    another try

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const goals = require("./Controllers/Goals")
 const goalsData = require("./Controllers/GoalsData")
 
 const db = knex({
-  client: "postgres",
+  client: "pg",
   connection: {
     connectionString: process.env.Database_URL,
     ssl: true,
